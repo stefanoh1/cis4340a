8738cd99b114381a542a9ba2883fd7799fa57ac9
stefanoh1 <105293332+stefanoh1@users.noreply.github.com>
2026-03-08 22:57:51 -0400

    Add files via upload

---
+++ cis4340.java
@@ -0,0 +1,23 @@
+/******************************************************************************
+ *  Compilation:  javac cis4340.java
+ *  Execution:    java cis4340
+ *
+ *  Prints 
+ *    "Hello, World - from Stefano Hernandez".
+ *  By tradition, this is everyone's first program.
+ *
+ * Revision History:
+ *   % java cis4340
+ *  Hello, World - from Stefano Hernandez
+ *  %
+ *
+ ******************************************************************************/
+
+public class cis4340 {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World - from Stefano Hernandez");
+    }
+
+}
