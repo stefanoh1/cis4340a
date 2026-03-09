d563d5cf41bcf858786e0224191fa6ee5f6e53b2
stefanoh1 <105293332+stefanoh1@users.noreply.github.com>
2026-03-08 23:13:55 -0400

    Update R05_OBJ03_J.java

--- R05_OBJ03_J.java
+++ R05_OBJ03_J.java
@@ -4,13 +4,13 @@ OBJ03-J. Prevent heap pollution
 */
 
 class ListUtility {
-  private static void addToList(List list, Object obj) {
-    list.add(obj); // Unchecked warning
+  private static void addToList(List<String> list, String str) {
+    list.add(str);     // No warning generated
   }
 
   public static void main(String[] args) {
     List<String> list = new ArrayList<String> ();
-    addToList(list, 42);
-    System.out.println(list.get(0));  // Throws ClassCastException
+    addToList(list, "42");
+    System.out.println(list.get(0));
   }
 }
