418d3acb9bf127c36f0b237d2745d84aed879506
stefanoh1 <105293332+stefanoh1@users.noreply.github.com>
2026-03-08 23:08:25 -0400

    Update R01_DCL00_J.java
    Reordered static field initialization to prevent class initialization cycles.

--- R01_DCL00_J.java
+++ R01_DCL00_J.java
@@ -5,9 +5,8 @@ DCL00-J. Prevent class initialization cycles
 
 public class Cycle {
   private final int balance;
-  private static final Cycle c = new Cycle();
   private static final int deposit = (int) (Math.random() * 100); // Random deposit
-
+  private static final Cycle c = new Cycle();  // Inserted after initialization of required fields
   public Cycle() {
     balance = deposit - 10; // Subtract processing fee
   }
